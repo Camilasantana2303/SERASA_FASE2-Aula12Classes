e98ccfa006c07e00ae91ea0aa1903485b97d1aba
camila.scarneiro1 <camila.scarneiro1@CAR0588302W10-1>
2023-08-18 13:53:06 -0300

    Adicionar arquivos de projeto.

---
+++ Fase2-Aula12Classes/Personagem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase2_Aula12Classes
+{
+    internal class Personagem
+    {
+        //variaveis(atributos)
+       public string nome;
+       public string raca;
+       public int poder;
+        
+        //funções(metodos)
+        public void mostra()
+        {
+            Console.WriteLine($"Seu personagem é {raca} o nome é {nome} e o poder é {poder}");
+        }
+
+    }
+}
---
+++ Fase2-Aula12Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Fase2_Aula12Classes;
+
+Console.WriteLine("Hello, World!");
+Personagem guerreiro= new Personagem();
+guerreiro.nome = "Camilaa";
+guerreiro.raca = "Humanoo";
+guerreiro.poder = 1000;
+guerreiro.mostra();
+
+Personagem mago= new Personagem();
+mago.raca = "Elfo";
+mago.poder = 100;
+mago.nome = "Harry";
+
+mago.mostra();
+
+
+Personagem bruxo = mago;
+bruxo.nome = "Hermione";
+bruxo.mostra();
+
+Personagem atiradora= new Personagem();
+
+atiradora.raca = "Imperatriz";
+atiradora.poder = 300;
+atiradora.nome = "Reyna";
+
+atiradora.mostra();
+
+
+
+Console.ReadKey();
